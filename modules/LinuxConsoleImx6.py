48f8dfb2e796b99b9bc65aa97460be77e0f16a8e
Andreas Müller <schnitzeltony@gmail.com>
2021-07-10 14:08:43 +0200

    Add some notifications on ZERA-specific services
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- modules/LinuxConsoleImx6.py
+++ modules/LinuxConsoleImx6.py
@@ -5,6 +5,10 @@ class LinuxConsoleImx6(SerialLoggerFilterNotify):
         searchEntries = [
             SerialLoggerFilterEntry('u-boot', ''),
             SerialLoggerFilterEntry(' login: ', 'Console login reached'),
+            SerialLoggerFilterEntry('Started Zera Module Manager.', 'Modulemanager started'),
+            SerialLoggerFilterEntry('Started Zera DSP daemon.', 'DSP service started'),
+            SerialLoggerFilterEntry('Started SEC1000 daemon', 'Error calculator service started'),
+            SerialLoggerFilterEntry('Started Zera Resource Manager.', 'Resource manager service started'),
             SerialLoggerFilterEntry('Error', '',
                                     ['cannot determine file size', 'ti-connectivity', 'regulatory.db'])
                          ]
