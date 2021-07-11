9e8e549befdcf6932656ae650cd2af0dea717b91
Andreas Müller <schnitzeltony@gmail.com>
2021-07-11 14:03:15 +0200

    ZeraDeviceLogger: remove unneeded imports
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- ZeraDeviceLogger.py
+++ ZeraDeviceLogger.py
@@ -4,9 +4,6 @@ import time
 from modules.KeyboardStopper import AbortSingleton
 from modules.LoggerFactory import LoggerFactory
 
-from modules.LoggerMT310s2SystemController import LoggerMT310s2SystemController
-from modules.LoggerLinuxConsoleImx6 import LoggerLinuxConsoleImx6
-
 keyboardStopper = AbortSingleton()
 loggerFactory = LoggerFactory('configurations/SerialLogger.json')
 
